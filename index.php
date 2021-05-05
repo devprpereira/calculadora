b97cf7f7fc3c910797bfd103f4f8286cf632e09e
pereirapr <paulo_roberto_pereira10@hotmail.com>
2021-05-05 00:59:13 -0300

    - Criado página de Index com um formulário simples para receber dois campos com valores inteiro.

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP and jQuery Calculator</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+    <form method="POST"> Formulário de Calculadora
+        <div class="row">
+            <label for="n1" > Primeiro Número: 
+                <input name="n1" id="n1"/>
+            </label>
+        </div>
+        <div class="row">
+            <label for="n2" > Segundo Número:
+                <input name="n2" id="n2"/>
+            </label>    
+        </div>
+        <button id="sendButton" type="submit">Calcular</button>
+    </form>
+</div>
+</body>
